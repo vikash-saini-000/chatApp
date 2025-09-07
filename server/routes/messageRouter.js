99fb14f51f224de563b89646c73065682c93d04a
vikash <vikashtondwals6@gmail.com>
2025-09-07 22:50:52 +0530

    upadte toogle point

--- server/routes/messageRouter.js
+++ server/routes/messageRouter.js
@@ -8,4 +8,5 @@ messageRouter.get("/users", protectRoute, getUsersForSidebar);
 messageRouter.get("/:id", protectRoute, getMessages);
 messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
 messageRouter.post("/send/:id", protectRoute,sendMessage);
+
 export default messageRouter;
